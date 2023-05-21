2c826bf464b16a75dd72c374bae34205fe06eed1
Sunny17082 <91121970+Sunny17082@users.noreply.github.com>
2023-05-21 13:31:10 +0530

    mergeSort.c

---
+++ mergeSort.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+void merge(int a[],int l,int mid,int h)
+{
+    int i=l,j=mid+1,k=l;
+    int b[h+1];
+    while(i<=mid && j<=h)
+    {
+        if(a[i]<a[j])
+            b[k++]=a[i++];
+        else
+            b[k++]=a[j++];
+    }
+    for(;i<=mid;i++)
+        b[k++]=a[i];
+    for(;j<=h;j++)
+        b[k++]=a[j];
+    for(i=l;i<=h;i++)
+        a[i]=b[i];
+}
+void mergeSort(int arr[],int l,int h)
+{
+    int mid;
+    if(l<h)
+    {
+        mid=(l+h)/2;
+        mergeSort(arr,l,mid);
+        mergeSort(arr,mid+1,h);
+        merge(arr,l,mid,h);
+    }
+}
+int main()
+{
+    int arr[100];
+    int n;
+    printf("Enter Size\n");
+    scanf("%d",&n);
+    printf("Enter elements\n");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    mergeSort(arr,0,n-1);
+    printf("Sorted array:\n");
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
